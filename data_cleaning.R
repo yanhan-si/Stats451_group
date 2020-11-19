f887acc98974e2ef88d1e75cbdea456993bd1222
Zhen Yang <56163280+zhen-yang8@users.noreply.github.com>
2020-11-20 01:17:21 +0800

    Add data files and data cleaning code
    The output file is "./bitcoin.csv".

---
+++ data_cleaning.R
@@ -0,0 +1,38 @@
+# Data Cleaning
+# Author: Zhen Yang
+# Created by: Nov 20, 2020
+# Updated by: Nov 20, 2020
+# --------------------------------------------------------------------------------
+
+library(tidyverse)
+
+# read data
+directory = "./archive/"
+bitcoin_price = read_csv(paste0(directory, "bitcoin_price.csv"))
+bitcoin_dataset = read_csv(paste0(directory, "bitcoin_dataset.csv"))
+
+# format the date
+## This will give NA(s) in some locales; uncomment the code below.
+## lct <- Sys.getlocale("LC_TIME"); Sys.setlocale("LC_TIME", "C")
+bitcoin_price = bitcoin_price %>% mutate(Date = as.Date(Date, "%b %d, %Y"))
+bitcoin_dataset = mutate(bitcoin_dataset, Date = as.Date(Date))
+
+# join two datasets
+bitcoin = bitcoin_price %>% left_join(bitcoin_dataset)
+
+# process missing values
+# 1. Volume
+## We notice that in bitcoin_price dataset, Volume is the only column 
+## having missing values from 2013-04-28 to 2013-12-26; 
+## uncomment the code below to see the missing rows.
+## bitcoin_price %>% filter(is.na(Volume)) %>% arrange(Date)
+bitcoin = bitcoin %>% filter(!is.na(Volume))
+
+# 2. btc_trade_volume - undone
+## Rows having missing btc_trade_volume are scattered from someday in 2014 to somday in 2015;
+## For now, we do not process them and leave us with 13 missing values of btc_trade_volume;
+## uncomment the code below to see the missing rows.
+## bitcoin[!complete.cases(bitcoin), ] %>% arrange(Date)
+
+# write output
+write_csv(bitcoin, "bitcoin.csv")
