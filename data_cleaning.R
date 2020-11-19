df0588b1a3926a7cf865ee52f2e9038a46fe2979
yanhan-si <syanhan96@gmail.com>
2020-11-19 15:17:36 -0500

    Add cols-log_close, log_return, ret
    Rewrite the output bitcoin.csv

--- data_cleaning.R
+++ data_cleaning.R
@@ -2,6 +2,7 @@
 # Author: Zhen Yang
 # This bitcoin_dataset  contains info from 2010 to 2018. (Row: 2920)
 # The bitcoin_price  contains info from 2013 to 2018. (Row: 1760)
+# Update: Add log_close, log_return, ret.
 # Created by: Nov 20, 2020
 # Updated by: Nov 20, 2020
 # --------------------------------------------------------------------------------
@@ -28,11 +29,23 @@ bitcoin = bitcoin_price %>% left_join(bitcoin_dataset, by = "Date")
 ## having missing values from 2013-04-28 to 2013-12-26; 
 ## uncomment the code below to see the missing rows.
 ## bitcoin_price %>% filter(is.na(Volume)) %>% arrange(Date) 
+## bitcoin[!complete.cases(bitcoin),]
 
-bitcoin[!complete.cases(bitcoin),]
 bitcoin = bitcoin %>% filter(!is.na(Volume))
 
-# 2. btc_trade_volume - undone
+bitcoin = bitcoin %>% arrange(Date)
+
+log_close = log(bitcoin$Close)
+log_return = diff(log_close)
+ret = exp(log_return) - 1
+bitcoin = bitcoin %>% filter(Date != "2013-12-27") 
+bitcoin$log_return = log_return
+bitcoin$ret = ret
+bitcoin$log_close = log(bitcoin$Close)
+
+names(bitcoin)
+
+  # 2. btc_trade_volume - undone
 ## Rows having missing btc_trade_volume are scattered from someday in 2014 to somday in 2015;
 ## For now, we do not process them and leave us with 13 missing values of btc_trade_volume;
 ## uncomment the code below to see the missing rows.
