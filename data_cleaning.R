a3cd6104a5e5b2ff12b7eaa03fdc02a06dc7ce08
yanhan-si <syanhan96@gmail.com>
2020-11-19 14:53:30 -0500

    code format change
    No changes on the output dataset.

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,5 +1,7 @@
 # Data Cleaning
 # Author: Zhen Yang
+# This bitcoin_dataset  contains info from 2010 to 2018. (Row: 2920)
+# The bitcoin_price  contains info from 2013 to 2018. (Row: 1760)
 # Created by: Nov 20, 2020
 # Updated by: Nov 20, 2020
 # --------------------------------------------------------------------------------
@@ -15,17 +17,19 @@ bitcoin_dataset = read_csv(paste0(directory, "bitcoin_dataset.csv"))
 ## This will give NA(s) in some locales; uncomment the code below.
 ## lct <- Sys.getlocale("LC_TIME"); Sys.setlocale("LC_TIME", "C")
 bitcoin_price = bitcoin_price %>% mutate(Date = as.Date(Date, "%b %d, %Y"))
-bitcoin_dataset = mutate(bitcoin_dataset, Date = as.Date(Date))
+bitcoin_dataset = bitcoin_dataset %>% mutate(Date = as.Date(Date, "%b %d, %Y"))
 
-# join two datasets
-bitcoin = bitcoin_price %>% left_join(bitcoin_dataset)
+# join two datasets by date
+bitcoin = bitcoin_price %>% left_join(bitcoin_dataset, by = "Date")
 
 # process missing values
 # 1. Volume
 ## We notice that in bitcoin_price dataset, Volume is the only column 
 ## having missing values from 2013-04-28 to 2013-12-26; 
 ## uncomment the code below to see the missing rows.
-## bitcoin_price %>% filter(is.na(Volume)) %>% arrange(Date)
+## bitcoin_price %>% filter(is.na(Volume)) %>% arrange(Date) 
+
+bitcoin[!complete.cases(bitcoin),]
 bitcoin = bitcoin %>% filter(!is.na(Volume))
 
 # 2. btc_trade_volume - undone
