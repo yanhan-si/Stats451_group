48447cf5fbb8c06e35dc319a6817543b353b54cc
Zhen Yang <393661355@qq.com>
2020-11-21 00:42:34 +0800

    Update data cleaning

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,10 +1,10 @@
 # Data Cleaning
-# Author: Zhen Yang
+#
 # This bitcoin_dataset  contains info from 2010 to 2018. (Row: 2920)
 # The bitcoin_price  contains info from 2013 to 2018. (Row: 1760)
-# Update: Add log_close, log_return, ret.
+#
 # Created by: Nov 20, 2020
-# Updated by: Nov 20, 2020
+# Updated by: Nov 21, 2020
 # --------------------------------------------------------------------------------
 
 library(tidyverse)
@@ -45,11 +45,26 @@ bitcoin$log_close = log(bitcoin$Close)
 
 names(bitcoin)
 
-  # 2. btc_trade_volume - undone
+# 2. btc_trade_volume - undone
 ## Rows having missing btc_trade_volume are scattered from someday in 2014 to somday in 2015;
 ## For now, we do not process them and leave us with 13 missing values of btc_trade_volume;
 ## uncomment the code below to see the missing rows.
 ## bitcoin[!complete.cases(bitcoin), ] %>% arrange(Date)
 
+# plot the close price against date
+ggplot(data = bitcoin, mapping = aes(x = bitcoin$Date, y = bitcoin$Close)) +
+  geom_line() +
+  theme_classic()
+## We find that the time chain is too long to fit in R,
+## so we decide to cut it off at year 2017.
+bitcoin = bitcoin %>% filter(Date >= '2017-01-01') %>% arrange(Date)
+
+# split the train (80%) and test (20%) dataset
+n_train = ceiling(0.8*nrow(bitcoin))
+bitcoin_train = bitcoin[1:n_train, ]
+bitcoin_test = bitcoin[(n_train+1):nrow(bitcoin), ]
+
 # write output
 write_csv(bitcoin, "bitcoin.csv")
+write_csv(bitcoin_train, "bitcoin_train.csv")
+write_csv(bitcoin_test, "bitcoin_test.csv")
