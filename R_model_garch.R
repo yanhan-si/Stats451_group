f532bdf39529c85eaa2c63e17c587a9639a15354
yanhan-si <syanhan96@gmail.com>
2020-11-21 10:18:11 -0500

    Create R_model_garch.R

---
+++ R_model_garch.R
@@ -0,0 +1,11 @@
+library(loo)
+library(rstan)
+garch11_setup <- rstan::stan_model(file = 'model_garch.stan')
+dat = read.csv("bitcoin_train.csv")
+
+stan_data <- list(y = dat$log_return,N = length(y),sigma1 = 0.01)
+
+garch11 <- rstan::sampling(garch11_setup, data = stan_data)
+
+garch11_fit <- rstan::extract(garch11, permuted = TRUE)
+
