28acaaece1b4f7e5d3f82b9830475839e7437629
yanhan-si <syanhan96@gmail.com>
2020-11-21 10:48:11 -0500

    Update R_model_garch.R

--- R_model_garch.R
+++ R_model_garch.R
@@ -1,9 +1,10 @@
 library(loo)
 library(rstan)
-garch11_setup <- rstan::stan_model(file = 'model_garch.stan')
-dat = read.csv("bitcoin_train.csv")
-
-stan_data <- list(y = dat$log_return,N = length(y),sigma1 = 0.01)
+garch11_setup <- rstan::stan_model(file = './Stan/model_garch.stan')
+dat = read.csv("./data/bitcoin_train.csv")
+y = dat$log_return
+N = length(y)
+stan_data <- list(y = y,N = N,sigma1 = 0.01)
 
 garch11 <- rstan::sampling(garch11_setup, data = stan_data)
 
@@ -12,8 +13,9 @@ garch11_fit <- rstan::extract(garch11, permuted = TRUE)
 plot(garch11_fit$alpha1, type = "l")
 plot(garch11_fit$mu, type = "l")
 
-log_lik_1 <- extract_log_lik(garch11, merge_chains = FALSE)
-r_eff_1 <- relative_eff(exp(log_lik_1), cores = 2)
-loo_1 <- loo(log_lik_1, r_eff = r_eff_1, cores = 2)
-print(loo_1)
-plot(loo_1, main = "PSIS diagnostic plot( Seperate model )")
+# Updates needed for computing the log likelihood
+# log_lik_1 <- extract_log_lik(garch11, merge_chains = FALSE)
+# r_eff_1 <- relative_eff(exp(log_lik_1), cores = 2)
+# loo_1 <- loo(log_lik_1, r_eff = r_eff_1, cores = 2)
+# print(loo_1)
+# plot(loo_1, main = "PSIS diagnostic plot( Seperate model )")
